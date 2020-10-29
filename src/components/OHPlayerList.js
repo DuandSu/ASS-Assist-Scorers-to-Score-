3642155a015add0ed8bb8b509a3d3fd60034660d
DuandSu <duane.w.munro@gmail.com>
2020-10-29 01:10:28 -0600

    28-Oct-2020: ASS: Fixed Checkbox Fix. Fixed Globals. Working on player list

---
+++ src/components/OHPlayerList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function OHPlayerList() {
+
+    
+    return (
+        <div>
+          {listOfUsers}
+        </div>
+      );
+}
--- src/components/OhHell.js
+++ src/components/OhHell.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './OhHell.css';
 
-let maxCardsCheck = 51;
-let passedInputNoPlayers = false;
-let passedInputNoPlayerCards = false;
-let screwTheDealer = false;
-
 class OhHell extends React.Component {
 
     constructor(props) {
         super(props);
+        this.maxCardsCheck = 51;
+        this.passedInputNoPlayers = false;
+        this.screwTheDealer = false;
         this.onInputchange = this.onInputchange.bind(this);
         this.onSubmitForm = this.onSubmitForm.bind(this);
         this.state = {
@@ -20,74 +18,69 @@ class OhHell extends React.Component {
     }
 
     onInputchange(event) {
-      console.log("Handling Input Change:");
       console.log(event.target.name + "=" + event.target.value);
-      
       this.setState({
         [event.target.name]: event.target.value
       });
 
       if (event.target.name === "checkScrewTD") {
-        if (event.target.name.checked === true) screwTheDealer = true;
-        else screwTheDealer = false;
-
-        console.log("Screw the Dealer Flag: " + screwTheDealer);
-        
+        this.screwTheDealer = event.target.checked;
         this.setState({
-          checkScrewTD: screwTheDealer,
+          // checkScrewTD: this.screwTheDealer,
           msgArea: ""
         });
+        console.log("Screw the Dealer Flag: " + this.screwTheDealer);
       }
 
       if (event.target.name === "inputNoPlayers") {
         if (event.target.value >= 2 && event.target.value <= 51) {
           let noCards = Math.floor(52 / event.target.value);
           if (52 % event.target.value === 0) noCards--;
-          maxCardsCheck = noCards;
+          this.maxCardsCheck = noCards;
 
           this.setState({
             inputNoPlayerCards: noCards,
             msgArea: ""
           });
-          passedInputNoPlayers = true;
-          passedInputNoPlayerCards = true;
+          this.passedInputNoPlayers = true;
+          this.passedInputNoPlayers = true;
         }
         else {
           this.setState({
             msgArea: "Number of Players MUST be in the range 2 to 51"
           });
-          passedInputNoPlayers = false;          
+          this.passedInputNoPlayers = false;          
         }
       }
 
       if (event.target.name === "inputNoPlayerCards") {
-        if (event.target.value > 1 && event.target.value <= maxCardsCheck) {
+        if (event.target.value > 1 && event.target.value <= this.maxCardsCheck) {
           this.setState({
             inputNoPlayerCards: event.target.value,
             msgArea: ""
           });
-          passedInputNoPlayerCards = true;
+          this.passedInputNoPlayers = true;
         }
         else {
           this.setState({
-            msgArea: `Number of Cards cannot be lower than 1 and cannot exceed ${maxCardsCheck} with chosen number of Players`
+            msgArea: `Number of Cards cannot be lower than 1 and cannot exceed ${this.maxCardsCheck} with chosen number of Players`
           });          
-          passedInputNoPlayerCards = false;
+          this.passedInputNoPlayers = false;
         }
       }
     }
     
     onSubmitForm() {
-      if (passedInputNoPlayers && passedInputNoPlayerCards) {
+      if (this.passedInputNoPlayers && this.passedInputNoPlayers) {
         console.log(this.state)
       }
       else {
-        if (!passedInputNoPlayers) {
+        if (!this.passedInputNoPlayers) {
           this.setState({
             msgArea: `Correct error on Number of Players before creating`          
           });
         }
-        else if (!passedInputNoPlayerCards) {
+        else if (!this.passedInputNoPlayers) {
           this.setState({
             msgArea: `Correct error on Number of Cards per Player before creating`          
           });
