4e656c98c83b050dfa3bd8d42cbab681b4298672
DuandSu <duane.w.munro@gmail.com>
2020-12-06 14:14:59 -0700

    6-Dec-2020: ASS: Done dynamic Scoring Totals component, but add actual totals later

--- src/components/OHScoringTotals.js
+++ src/components/OHScoringTotals.js
@@ -2,49 +2,29 @@ import React from 'react';
 
 function OHScoringTotals(props) {
 
-    let totalsOH = 
+    let sumsOH = [];
+    for (let playerNo = 0; playerNo < props.listOfPlayers.length; playerNo++) {
+        sumsOH.push(
+            <label key={`labelOHBidSums${playerNo}`} className="OhHellTotals2BB">Junk</label>
+        );
+        sumsOH.push(
+            <label key={`labelOHMadeSums${playerNo}`} className="OhHellTotals2BB">Junk</label>
+        );
+        sumsOH.push(
+            <label key={`labelOHPtsSums${playerNo}`} className="OhHellTotalsBB">34</label>
+        );
+    }
+
+    let totalsOH = [];
+    totalsOH.push(
         <div>
             <label className="OhHellTotalsLBB">Total</label>
             <label className="OhHellTotals2BB">Junk</label>
-            <label className="OhHellTotals2BB">Junk</label>
-            <label className="OhHellTotals2BB">Junk</label>
-            <label className="OhHellTotalsBB">34</label>
-            <label className="OhHellTotals2BB">Junk</label>
-            <label className="OhHellTotals2BB">Junk</label>
-            <label className="OhHellTotalsBB">34</label>
-            <label className="OhHellTotals2BB">Junk</label>
-            <label className="OhHellTotals2BB">Junk</label>
-            <label className="OhHellTotalsBB">43</label>
-            <label className="OhHellTotals2BB">Junk</label>
-            <label className="OhHellTotals2BB">Junk</label>
-            <label className="OhHellTotalsBB">55</label>
-            <label className="OhHellTotals2BB">Junk</label>
-            <label className="OhHellTotals2BB">Junk</label>
-            <label className="OhHellTotalsBB">64</label>
-            <label className="OhHellTotals2BB">Junk</label>
-            <label className="OhHellTotals2BB">Junk</label>
-            <label className="OhHellTotalsBB">43</label>
+            {sumsOH}
             <label className="OhHellTotalsRBB">Junk</label>
             <br></br>
         </div>
-
-    // header1.push(
-    //     <div>
-    //     </div>
-    // );
-    // for (let playerNo = 0; playerNo < props.listOfPlayers.length; playerNo++) {
-    //     playerList.push(
-    //         <div key={`divPlayerList${playerNo}`}>
-    //             <label htmlFor={`inputPlayer${playerNo}`}>Enter Player {playerNo+1}: </label>
-    //             <input 
-    //                 name={`inputPlayer${playerNo}`}
-    //                 type="text"
-    //                 value={props.listOfPlayers[playerNo]}
-    //                 onChange={props.onChange}>
-    //             </input>            
-    //         </div>
-    //     );
-    // }
+    );
 
     return (
         <div>
